a338e3e088d3ecb748699ca96715f6a99abdcde1
EloraBarua <elorabarua62@gmail.com>
2022-03-12 00:50:42 +0600

    change2

--- house_rental/urls.py
+++ house_rental/urls.py
@@ -19,7 +19,6 @@ from accounts import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-from accounts.views import review_submit
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
